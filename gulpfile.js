749fa2164b8ef3f10f99cc97905635aac330576a
Cameron Brandon White <cameron.white@janrain.com>
2014-12-27 03:43:29 -0800

    Fixed build script, browserify module, and renamed

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var run = require('gulp-run');
 
 var pegjs_cmd = './node_modules/.bin/pegjs';
+var browserify_cmd = './node_modules/.bin/browserify';
 
 gulp.task('generate-parser', function() {
-    run('mkdir lib').exec();
-    run(pegjs_cmd + ' src/parser.pegjs lib/WeberLogicJS.js').exec();
+    gulp.src('src/WeberLogic.js')
+        .pipe(run(pegjs_cmd))
+        .pipe(gulp.dest('build'));
+    run(browserify_cmd + ' -r ./build/WeberLogic.js:WeberLogic -o dist/WeberLogic.js').exec();
 });
+
+
